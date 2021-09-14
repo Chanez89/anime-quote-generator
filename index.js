58997020430cbf05039a6c3ee34bb0204f5b29ab
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-09-14 09:24:18 -0700

    Fetched API

---
+++ index.js
@@ -0,0 +1,35 @@
+const BASE_URL = 'https://animechan.vercel.app/api/random'
+
+
+fetch(BASE_URL)
+    .then(response => response.json())
+    .then(quote => console.log(quote))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
